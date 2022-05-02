bc36b92803436b680f74d1068a8dc9bdcb26e802
Nikita Ilyanov <nikita.glk@mail.ru>
2022-05-02 11:44:01 +0300

    feat: complete stack

--- src/stack.js
+++ src/stack.js
@@ -12,20 +12,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
+
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor () { //инициализация стека
+    this.stack = [];
+  }
+  push(item) {
+    this.stack.push(item);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[this.stack.length - 1];
   }
 }
 
